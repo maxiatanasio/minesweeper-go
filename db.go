36b4e098e8e64b5e4735f011bfd319f4b1c2eb37
Maxi Atanasio <maximilianoatanasio@gmail.com>
2020-04-17 16:56:06 -0300

    Add envvars for DB config

--- db.go
+++ db.go
@@ -8,7 +8,7 @@ import (
 )
 
 func ConfigDB() *gorm.DB {
-	db, err := gorm.Open("mysql", "root:12345678@tcp(localhost)/minescrapper?parseTime=True")
+	db, err := gorm.Open("mysql", os.Getenv("DB_STRING"))
 	if err != nil {
 		print("There was a problem connecting to the DB", err.Error())
 		os.Exit(1)
